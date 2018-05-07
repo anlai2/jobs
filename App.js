d9a60640dc77563e7a7aee7a84f73e0fcac5be25
Andy <anlaics2@gmail.com>
2018-05-07 09:12:21 -0700

    initialized redux

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
 import DeckScreen from './screens/DeckScreen';
 import SettingsScreen from './screens/SettingsScreen';
-import ReviewScreen from './screens/ReviewScreen'; 
+import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
   render() {
@@ -16,20 +18,22 @@ export default class App extends React.Component {
       auth: { screen: AuthScreen },
       main: {
         screen: createBottomTabNavigator({
-			map: { screen: MapScreen},
-			deck: { screen: DeckScreen },
-			review: { 
-				screen: createStackNavigator({
-					review: { screen: ReviewScreen },
-					settings: { screen: SettingsScreen }
-				})
-			}
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: createStackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
         })
       }
     });
 
     return (
-      <MainNavigator />
+      <Provider store={store}>
+        <MainNavigator />
+      </Provider>
     );
   }
 }
