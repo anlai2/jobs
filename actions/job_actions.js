7ce931bdfaf79bdda7290c6e7e6c1924be471876
Andy <anlaics2@gmail.com>
2018-05-09 12:05:16 -0700

    deck screen displays nearby jobs

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -20,14 +20,15 @@ const buildJobsUrl = (zip) => {
     return `${JOB_ROOT_URL}${query}`;
 };
 
-export const fetchJobs = (region) => async (dispatch) => {
+export const fetchJobs = (region, callback) => async (dispatch) => {
     try {
         let zip = await reverseGeocode(region);
         const url = buildJobsUrl(zip);
         let { data } = await axios.get(url);
-        dispatch({ type: FETCH_JOBS, payload: data })
-        console.log(data);
+        dispatch({ type: FETCH_JOBS, payload: data });
+        callback();
     } catch (err) {
         console.log(err);
     }
 };
+
